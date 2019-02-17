7ba1b660757d6a5b746d3df0c179b144c32f91f6
Lucas Salvino <lucassalvino1@gmail.com>
2019-02-17 19:54:23 -0300

    Import

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+#include "Teste/runteste.h"
+int main(void)
+{
+    RunTeste teste;
+    teste.executaTestesGrafo();
+    return 0;
+}
